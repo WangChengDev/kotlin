10431b181ca5d3ee9167effdb1e042fa75954fdb
Mikhael Bogdanov <mikhael.bogdanov@jetbrains.com>
2016-12-02 12:18:25 +0100

    Updated NON_PUBLIC_CALL_FROM_PUBLIC_INLINE diagnostic to support @PublishedApi

--- compiler/tests/org/jetbrains/kotlin/checkers/DiagnosticsTestGenerated.java
+++ compiler/tests/org/jetbrains/kotlin/checkers/DiagnosticsTestGenerated.java
@@ -10612,6 +10612,12 @@ public class DiagnosticsTestGenerated extends AbstractDiagnosticsTest {
                 doTest(fileName);
             }
 
+            @TestMetadata("publishedApi.kt")
+            public void testPublishedApi() throws Exception {
+                String fileName = KotlinTestUtils.navigationMetadata("compiler/testData/diagnostics/tests/inline/publishedApi.kt");
+                doTest(fileName);
+            }
+
             @TestMetadata("recursion.kt")
             public void testRecursion() throws Exception {
                 String fileName = KotlinTestUtils.navigationMetadata("compiler/testData/diagnostics/tests/inline/recursion.kt");
@@ -10887,6 +10893,12 @@ public class DiagnosticsTestGenerated extends AbstractDiagnosticsTest {
                 @TestMetadata("localFun.kt")
                 public void testLocalFun() throws Exception {
                     String fileName = KotlinTestUtils.navigationMetadata("compiler/testData/diagnostics/tests/inline/nonPublicMember/localFun.kt");
+                    doTest(fileName);
+                }
+
+                @TestMetadata("publishedApi.kt")
+                public void testPublishedApi() throws Exception {
+                    String fileName = KotlinTestUtils.navigationMetadata("compiler/testData/diagnostics/tests/inline/nonPublicMember/publishedApi.kt");
                     doTest(fileName);
                 }
             }
